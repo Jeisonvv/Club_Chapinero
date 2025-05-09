cacba164a8b9e00d72a0e0ecf974758fbe1129b7
jeisonvv <jeisonhvargaav@gmail.com>
2025-05-08 19:10:54 -0500

    feat: Add footer component and global styles; update layout and services section
    - Introduced a new Footer component with styling and SVG icon.
    - Updated the Layout component to include the Footer.
    - Modified the Welcome component to use 'prose' class for services section.
    - Enhanced the services page layout with additional styling.
    - Added autoprefixer and postcss to package.json for better CSS compatibility.
    - Cleaned up global CSS file with additional spacing.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
-// https://astro.build/config
+
 export default defineConfig({
-  
-  integrations: [react()], // Integra React con Astro
+  integrations: [react()],
 });
